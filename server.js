44edea34d51c350c45f47104328e48edb15acd83
rohit prabhat <rohitprabhat@rohits-MacBook-Air.local>
2019-02-09 23:35:48 +0530

    error resolved

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+// app.use((req, res, next) => {
+//   var now = new Date().toString();
+//   var log = `${now}: ${req.method} ${req.url}`;
 
-  console.log(log);
-  fs.appendFile('server.log', log + '\n');
-  next();
-});
+//   console.log(log);
+//   fs.appendFile('server.log', log + '\n');
+//   next();
+// });
 
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
